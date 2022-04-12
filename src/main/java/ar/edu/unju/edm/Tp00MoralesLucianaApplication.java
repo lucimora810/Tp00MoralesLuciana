51243a9e2d3ebc35a65488d3a0de5c8f53d259da
IVANA <IVANA@10.0.5.152>
2022-04-12 14:41:41 -0300

    resuelto tp

--- src/main/java/ar/edu/unju/edm/Tp00MoralesLucianaApplication.java
+++ src/main/java/ar/edu/unju/edm/Tp00MoralesLucianaApplication.java
@@ -10,4 +10,36 @@ public class Tp00MoralesLucianaApplication {
 		SpringApplication.run(Tp00MoralesLucianaApplication.class, args);
 	}
 
+	
+}
+
+paquete  ar . educación _ unju _ educación _ aplicación ;
+
+importar  ar . educación _ unju _ edm . modelo _ calculadora ;
+//importar ar.edu.unju.edm.modelo.Estudiante;
+
+importar  organización . estructura de resorte . bota _ Aplicación Primavera ;
+importar  organización . estructura de resorte . bota _ autoconfigurar _ Aplicación SpringBoot ;
+
+@SpringBootApplication _
+public  class  Tpn00QuinterosmarcosApplication {
+
+	public  static  void  main ( String [] args ) {
+		
+		SpringAplicattion.run(Tp00MoralesLucianaAplicattion.class, args);
+
+	     Calculadora unCalculadora = new Calculadora  ( ); 
+	    DesCalculadora . conjuntoNúmero1 ( 10 );
+	    DesCalculadora . conjuntoNumero2 ( 2 );
+	    sistema _ fuera _ println ( "El resultado de la Suma es: " + unCalculadora . ResolverSuma ());
+	    sistema _ fuera _ println ( "El resultado de la Resta es: " + unCalculadora . ResolverResta ());
+	    sistema _ fuera _ println ( "El resultado de la Multiplicacion es: " + unCalculadora . ResolverMulti ());
+	    sistema _ fuera _ println ( "El resultado de la Division es: " + unCalculadora . ResolverDivision ());
+	    sistema _ fuera _ println ( "El resultado de la Potencia es: " + unCalculadora . ResolverPotencia ());
+	    sistema _ fuera _ println ( "El resultado de la Division es: " + unCalculadora . ResolverRaiz ());
+
+	
+	}
+
+
 }
